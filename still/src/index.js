a1109b5343ec060b529d7007265997d3c881b040
Mark Strader <straderm@umich.edu>
2017-06-29 20:30:55 -0400

    Updates

--- still/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
